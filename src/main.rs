76471937626bfdcd735d93b40ddad019248ab0ea
gws <glennws@pm.me>
2025-04-25 23:36:29 +0200

    connect and checkout services

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
+use std::time::Duration;
 
-use bluest::Adapter;
+use bluest::{btuuid, Adapter};
 use futures_lite::StreamExt;
 use tracing::info;
 use tracing::metadata::LevelFilter;
@@ -25,22 +26,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
     adapter.wait_available().await?;
 
     info!("starting scan");
-    let mut scan = adapter.scan(&[]).await?;
+    let service_filter = [btuuid::bluetooth_uuid_from_u16(0x185A)];
+    let mut scan = adapter.scan(&service_filter).await?;
     info!("scan started");
-    while let Some(discovered_device) = scan.next().await {
+    while let Some(device) = scan.next().await {
         info!(
-            "{}{}: {:?}",
-            discovered_device
-                .device
-                .name()
-                .as_deref()
-                .unwrap_or("(unknown)"),
-            discovered_device
+            "{:?} {}{}: {:?}",
+            device.device.id(),
+            device.device.name().as_deref().unwrap_or("(unknown)"),
+            device
                 .rssi
                 .map(|x| format!(" ({}dBm)", x))
                 .unwrap_or_default(),
-            discovered_device.adv_data.services
+            device.adv_data.services
         );
+        adapter.connect_device(&device.device).await?;
+        info!("connected!");
+
+        let services = device.device.services().await?;
+
+        for service in services {
+            info!("  {:?}", service);
+        }
+
+        tokio::time::sleep(Duration::from_secs(3)).await;
+
+        adapter.disconnect_device(&device.device).await?;
+        info!("disconnected!");
     }
 
     Ok(())
